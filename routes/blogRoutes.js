22c63cc4d90e15ce81ca3ed1ff759893a7884562
olrightythen <sulavneupane1905@gmail.com>
2024-09-06 17:10:08 +0545

    fixes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const { renderHomePage, renderSingleBlog, renderCreateBlog, createBlog, renderEditBlog, updateBlog, deleteBlog } = require("../controllers/blogController");
+const {
+  renderHomePage,
+  renderSingleBlog,
+  renderCreateBlog,
+  createBlog,
+  renderEditBlog,
+  updateBlog,
+  deleteBlog,
+} = require("../controllers/blogController");
 const upload = require("../middleware/multerConfig");
 
 router.route("/").get(renderHomePage);
 router.route("/blogs/:id").get(renderSingleBlog);
-router.route("/create").get(renderCreateBlog).post(upload.single("image"), createBlog);
-router.route("/edit/:id").get(renderEditBlog).put(upload.single("image"), updateBlog);
-router.route("/delete/:id").delete(deleteBlog);
+router
+  .route("/create")
+  .get(renderCreateBlog)
+  .post(upload.single("image"), createBlog);
+router
+  .route("/blogs/:id/edit")
+  .get(renderEditBlog)
+  .post(upload.single("image"), updateBlog);
+router.route("/blogs/:id/delete").post(deleteBlog);
 
 module.exports = router;
